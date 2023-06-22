1e9abfa306d2cd328dde2779857785da2dd52f3a
spencer0124 <spencer0124@naver.com>
2023-06-22 17:17:07 +0900

    feat: modul.css to tailwind#1

--- src/components/Top1.jsx
+++ src/components/Top1.jsx
@@ -3,12 +3,24 @@ import logo from "../assets/main_logo_eng.png";
 
 function Top1(props) {
   return (
-    <div className="flex items-center h-28 pl-5 pr-5 bg-white">
-      <div className="p-5">
-        <img src={logo} alt="Logo" className="h-auto max-w-41 max-h-28" />
+    <div className="flex justify-between items-center bg-white h-[139.49px]">
+      <div className="flex items-center">
+        <img
+          src={logo}
+          alt="SKKU logo"
+          className="h-10 pl-[64px] pr-[16px] pb-1"
+        />
+        <div className="w-0.5  bg-[#DDDDDD] h-10 "></div>
+        <span
+          className="pl-[16px] text-[28px] text-[#1b1b1b] font-gugi"
+          style={{ whiteSpace: "nowrap" }}
+        >
+          킹고코인
+        </span>
       </div>
-      <div className="p-5 ml-auto text-right">
-        소프트웨어학과&nbsp; | &nbsp;재학중&nbsp; | &nbsp;조승용&nbsp; |
+      <div className="pr-[62.02px] text-[20px] text-right link_text font-noto-sans-kr font-regular">
+        소프트웨어학과&nbsp;&nbsp; |&nbsp;&nbsp; 율전이&nbsp;&nbsp;
+        |&nbsp;&nbsp; O
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gugi: ["Gugi", "cursive"],
+        "noto-sans-kr": ["Noto Sans KR", "sans-serif"],
+      },
+      fontWeight: {
+        thin: 100,
+        light: 300,
+        regular: 400,
+        medium: 500,
+        bold: 700,
+        black: 900,
+      },
+    },
   },
   plugins: [],
 };
