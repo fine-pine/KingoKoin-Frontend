e46059b4b56d880dbaab4f1e639951cb5d91ad5f
finePine <lsm3645@g.skku.edu>
2023-06-24 20:04:33 +0900

    chore: add dummpy data

---
+++ src/api.tsx
@@ -0,0 +1,22 @@
+export const koin = {
+  curr: 180,
+  used: 60,
+  all: 240,
+};
+
+export const koinDetails = [
+  {
+    pf_name: "온라인명륜당",
+    title: "온라인명륜당 회원가입",
+    plus: true,
+    point: 50,
+    createdAt: "2023-05-10T04:05:08.000Z",
+  },
+  {
+    pf_name: "온라인명륜당",
+    title: "온라인명륜당 회원가입",
+    plus: false,
+    point: 50,
+    createdAt: "2023-05-10T04:05:08.000Z",
+  },
+];
